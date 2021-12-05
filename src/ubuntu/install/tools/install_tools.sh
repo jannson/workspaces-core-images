f50e1ab5088db78317f9d1515aa59996ddc171c5
ks-bao <xiaobao@linkease.com>
2021-12-05 11:24:49 +0000

    build ok in hk

--- src/ubuntu/install/tools/install_tools.sh
+++ src/ubuntu/install/tools/install_tools.sh
@@ -2,6 +2,7 @@
 set -e
 
 echo "Install some common tools for further installation"
+
 if [ "${DISTRO}" == "centos" ] ; then
   yum install -y vim wget net-tools bzip2 python3
   wget http://mirror.ghettoforge.org/distributions/gf/el/7/gf/x86_64/wmctrl-1.07-17.gf.el7.x86_64.rpm
@@ -18,6 +19,12 @@ fi
 
 if [ "$DISTRO" = "ubuntu" ]; then
   #update mesa to latest
-  add-apt-repository ppa:kisak/kisak-mesa
+  mkdir -p /etc/apt/sources.list.d
+  echo "deb http://ppa.launchpad.net/kisak/kisak-mesa/ubuntu bionic main" > /etc/apt/sources.list.d/kisak-ubuntu-kisak-mesa-bionic.list
+  apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EB8B81E14DA65431D7504EA8F63F0F2B90935439
+  # add-apt-repository ppa:kisak/kisak-mesa
+  apt update
+  apt install -y ubuntu-keyring ubuntu-dbgsym-keyring
   apt full-upgrade -y
 fi
+
